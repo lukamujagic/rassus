5574fffc7bd208b03523b271d372f9e1a8eedb4e
Luka Mujagić <luka.mujagic@fer.hr>
2024-01-12 14:08:30 +0100

    Dodan registration server

---
+++ Labosi/Lab3/registration-server/src/main/java/hr/fer/tel/rassus/registrationserver/RegistrationServerApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.tel.rassus.registrationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationServerApplication.class, args);
+	}
+
+}
---
+++ Labosi/Lab3/registration-server/src/test/java/hr/fer/tel/rassus/registrationserver/RegistrationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.tel.rassus.registrationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
