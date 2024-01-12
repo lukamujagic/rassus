862572683f14f872ebb00a0de97afdee72a92660
Luka Mujagić <luka.mujagic@fer.hr>
2024-01-12 07:25:19 +0100

    Lab3 init

---
+++ Labosi/Lab3/humidity-microservice/src/main/java/hr/fer/tel/rassus/humiditymicroservice/HumidityMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.tel.rassus.humiditymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumidityMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumidityMicroserviceApplication.class, args);
+	}
+
+}
